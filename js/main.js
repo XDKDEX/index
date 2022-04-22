4b208d9036461a33a5fbea284f86cba3c485ac5d
XDKDEX <103232673+XDKDEX@users.noreply.github.com>
2022-04-22 21:43:35 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var xhr = new XMLHttpRequest();
+xhr.open('get', 'https://api.moedog.org/hitokoto/?encode=json');
+xhr.onreadystatechange = function () {
+    var data = JSON.parse(xhr.responseText);
+    var hitokoto = document.getElementById('hitokoto');
+    hitokoto.innerText = data.hitokoto;
+}
+xhr.send();
