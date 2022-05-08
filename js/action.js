897b8fbc0258160d11cfa00f856353be7f5b9ddc
XDKDEX <103232673+XDKDEX@users.noreply.github.com>
2022-05-08 19:19:39 +0800

    Add files via upload

---
+++ js/action.js
@@ -0,0 +1,8 @@
+function HOME(){
+    document.getElementById("right-HOME").style.display = "block"
+    document.getElementById("right-ABOUT").style.display = "none"
+}
+function ABOUT(){
+    document.getElementById("right-ABOUT").style.display = "block"
+    document.getElementById("right-HOME").style.display = "none"
+}
